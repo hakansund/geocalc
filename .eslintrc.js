ef83e19e02dc1a56ad90f2e6c652e630839c3ebf
Håkan Sund <hakan.sund@gmail.com>
2017-02-11 17:51:08 +0100

    Setup linting

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ],
+    "env": {
+      "mocha": true,
+      "node": true
+    },
+    "rules": {
+      "no-console": 0
+    }
+};
