c2963340b6e25cf964d8c109040c709ce2b338ff
Håkan Sund <hakan.sund@gmail.com>
2017-02-11 21:40:43 +0100

    Changed wording on tests

--- test/triangle.spec.js
+++ test/triangle.spec.js
@@ -4,11 +4,11 @@ const Triangle = require('../lib/triangle');
 
 describe('Triangle', () => {
   describe('Calculation of circumference', () => {
-    it('calculateCircumference() should return 3.414213562373095 if 1, 1 are passed in', () => {
+    it('calculateCircumference() should return 3.414213562373095 if called with parameters 1, 1', () => {
       expect(Triangle.calculateCircumference(1, 1)).to.equal(3.414213562373095);
     });
 
-    it('calculateCircumference() should return 6.82842712474619 if 2, 2 are passed in', () => {
+    it('calculateCircumference() should return 6.82842712474619 if called with parameters 2, 2', () => {
       expect(Triangle.calculateCircumference(2, 2)).to.equal(6.82842712474619);
     });
 
@@ -16,25 +16,25 @@ describe('Triangle', () => {
       expect(() => Triangle.calculateCircumference(1)).to.throw('Parameter missing or not a number!');
     });
 
-    it('calculateCircumference() should throw \'Parameter missing or not a number!\' if non-number parameters are passed in', () => {
+    it('calculateCircumference() should throw \'Parameter missing or not a number!\' if called with non-number parameter', () => {
       expect(() => Triangle.calculateCircumference('a', 1)).to.throw('Parameter missing or not a number!');
     });
   });
 
   describe('Calculation of area', () => {
-    it('calculateArea() should return 0.5 if 1, 1 are passed in', () => {
+    it('calculateArea() should return 0.5 if called with parameters 1, 1', () => {
       expect(Triangle.calculateArea(1, 1)).to.equal(0.5);
     });
 
-    it('calculateArea() should return 3 if 2, 2 are passed in', () => {
+    it('calculateArea() should return 3 if called with parameters 2, 2', () => {
       expect(Triangle.calculateArea(2, 2)).to.equal(2);
     });
 
-    it('calculateArea() should throw \'Parameter missing or not a number!\' if too few parameters are passed in', () => {
+    it('calculateArea() should throw \'Parameter missing or not a number!\' if called with one parameter', () => {
       expect(() => Triangle.calculateArea(1)).to.throw('Parameter missing or not a number!');
     });
 
-    it('calculateArea() should throw \'Parameter missing or not a number!\' if non-number parameters are passed in', () => {
+    it('calculateArea() should throw \'Parameter missing or not a number!\' if if called with non-number parameter', () => {
       expect(() => Triangle.calculateArea('a', 1)).to.throw('Parameter missing or not a number!');
     });
   });
